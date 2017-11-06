c0f1a2370ced05117705ce93e3050fd9e1272110
王俊 <820989774@qq.com>
2017-11-06 16:07:59 +0800

    no message

---
+++ get/GetHotels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetHotels
+//
+//  Created by 黄泾诚 on 2017/11/6.
+//  Copyright © 2017年 黄泾诚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
